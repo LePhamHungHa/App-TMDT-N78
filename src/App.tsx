22d13e09dcebc369db4cea7f040530b9ef3312c0
Cao Văn Vượng <146173412+CaoVanVuongDH21DTA@users.noreply.github.com>
2024-05-24 15:25:47 +0700

    demo

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <a className="App-link" href="https://store.epicgames.com/" > Epic </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
